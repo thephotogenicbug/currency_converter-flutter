c608df9d35b0f697d1b05922b2db724f1f6dd8a0
thephotogenicbug <navinkumarachari@gmail.com>
2024-01-22 14:14:36 +0530

    convert stateless class to stateful class

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,8 +1,24 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+// Create a variable that stores the converted currency value
+// Create a function that multiplies the value given by the textfield with 81
+// Store the value in the variable that we created
+// Display the variable
+
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() {
+    return _CurrencyConverterMaterialPageState();
+  }
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+
+   late double result;
+   final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +46,9 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(
+            Text(
+              result.toString(),
+              style: const TextStyle(
                 fontSize: 45,
                 fontWeight: FontWeight.bold,
                 color: Color.fromARGB(255, 255, 255, 255),
@@ -41,6 +57,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(10),
               child: TextField(
+                controller: textEditingController,
                 style: const TextStyle(
                   color: Colors.black,
                 ),
@@ -76,9 +93,8 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 ),
                 onPressed: () {
                   // debug, release, profile
-                  if (kDebugMode) {
-                    debugPrint('Button Clicked');
-                  }
+                  result = double.parse(textEditingController.text) * 81;
+                  build(context);
                 },
                 child: const Text('Convert'),
               ),
@@ -89,3 +105,95 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
     );
   }
 }
+
+// class CurrencyConverterMaterialPagee extends StatelessWidget {
+//   const CurrencyConverterMaterialPagee({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     double result = 0;
+//     final TextEditingController textEditingController = TextEditingController();
+
+//     final border = OutlineInputBorder(
+//       borderSide: const BorderSide(
+//         color: Colors.white,
+//         width: 2.0,
+//         style: BorderStyle.solid,
+//       ),
+//       borderRadius: BorderRadius.circular(5),
+//     );
+
+//     return Scaffold(
+//       backgroundColor: Colors.blueGrey,
+//       appBar: AppBar(
+//         backgroundColor: Colors.blueGrey,
+//         elevation: 0,
+//         title: const Text(
+//           "Currency Convertor",
+//           style: TextStyle(color: Colors.white),
+//         ),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Text(
+//               result.toString(),
+//               style: const TextStyle(
+//                 fontSize: 45,
+//                 fontWeight: FontWeight.bold,
+//                 color: Color.fromARGB(255, 255, 255, 255),
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(10),
+//               child: TextField(
+//                 controller: textEditingController,
+//                 style: const TextStyle(
+//                   color: Colors.black,
+//                 ),
+//                 decoration: InputDecoration(
+//                   hintText: 'Please enter the amount in USD',
+//                   hintStyle: const TextStyle(
+//                     color: Colors.black,
+//                   ),
+//                   prefixIcon: const Icon(
+//                     Icons.monetization_on,
+//                     color: Colors.black,
+//                   ),
+//                   filled: true,
+//                   fillColor: Colors.white,
+//                   focusedBorder: border,
+//                   enabledBorder: border,
+//                 ),
+//                 keyboardType: const TextInputType.numberWithOptions(
+//                   decimal: true,
+//                 ),
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(10.0),
+//               child: ElevatedButton(
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.black,
+//                   foregroundColor: Colors.white,
+//                   minimumSize: const Size(double.infinity, 50),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(5),
+//                   ),
+//                 ),
+//                 onPressed: () {
+//                   // debug, release, profile
+//                   result = double.parse(textEditingController.text) * 81;
+//                   build(context);
+//                 },
+//                 child: const Text('Convert'),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
